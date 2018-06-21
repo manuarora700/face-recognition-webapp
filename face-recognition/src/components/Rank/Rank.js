bff41f046aa210678378c2ca268567ed60d21b4f
Manu Arora <mannuarora700@gmail.com>
2018-06-21 12:44:07 +0530

    Added particleJS and completed form

---
+++ face-recognition/src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Rank = () => {
+	return (
+
+		<div>
+			<div className='white f3'>
+			{'Manu, Your current rank is...'}
+			</div>
+			<div className='white f1'>
+			{'#5'}
+			</div>
+		</div>
+
+	)
+}
+
+export default Rank;
