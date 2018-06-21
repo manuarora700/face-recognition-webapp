8a4b0970ac68bdf8008aa66736e61588f96f9431
Manu Arora <mannuarora700@gmail.com>
2018-06-21 14:48:10 +0530

    Code refactoring

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -28,8 +28,13 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+
   }
   onInputChange = (event) => {
     this.setState({input: event.target.value});
@@ -37,18 +42,12 @@ class App extends Component {
 
   onButtonSubmit = () => {
       this.setState({imageUrl: this.state.input});
-      app.models.predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
-      .then(
-      function(response) {
-        // do something with response
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      app.models
+        .predict(
+          Clarifai.FACE_DETECT_MODEL, 
+          this.state.input)
+        .then(response => this.calculateFaceLocation(response))
+        .catch(err => console.log(err));
   }
   render() {
     return (
