ac8bec5ef32e4462db6474cfa2e435868ae69a6f
Manu Arora <mannuarora700@gmail.com>
2018-06-24 01:20:06 +0530

    Building actual routes

--- face-recognition-api/server.js
+++ face-recognition-api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+
 app.get('/', (req, res) => {
 	res.send('this is working');
 })
@@ -11,3 +12,13 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
 	console.log("running okay! on port 3000");
 });
+
+
+
+/*
+/ --> res = this is working
+/signin --> POST = success/failure
+/register --> POST = user object
+/profile/:userId --> GET = user
+/image --> PUT --> user updated
+*/
