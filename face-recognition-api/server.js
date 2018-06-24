e2458a8b6b46a99082619f9483303111cbb3b4cc
Manu Arora <mannuarora700@gmail.com>
2018-06-25 01:44:12 +0530

    Building the fundamentals

--- face-recognition-api/server.js
+++ face-recognition-api/server.js
@@ -1,14 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+const database = {
+	users: [
+	{
+		id: '123',
+		name: 'Manu',
+		email: 'manu@gmail.com',
+		password: 'cookies',
+		entries: 0,
+		joined: new Date()
+	},
+	{
+		id: '124',
+		name: 'Sally',
+		email: 'sally@gmail.com',
+		password: 'bananas',
+		entries: 0,
+		joined: new Date()
+	}
+	]
+}
 
 app.get('/', (req, res) => {
-	res.send('this is working');
+	res.send(database.users);
 })
 
+//for signin
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password) {
+		res.json('Success');
+	} else {
+		res.status(400).json('Error logging in');
+	}
+})
 
-
+app.post('/register', (req,res) => {
+	const { email, name, password } = req.body
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length - 1]);
+})
 app.listen(3000, () => {
 	console.log("running okay! on port 3000");
 });
