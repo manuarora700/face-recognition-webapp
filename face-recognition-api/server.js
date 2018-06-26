d151953bfc5cf90d4c9a488594f62aab740e01e2
Manu Arora <mannuarora700@gmail.com>
2018-06-26 23:13:44 +0530

    Bug fixes

--- face-recognition-api/server.js
+++ face-recognition-api/server.js
@@ -2,12 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'root', // change if necessary
+    database : 'face-recognition'
+  }
+});
+
 
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
 const database = {
 	users: [
 	{
@@ -53,43 +66,47 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req,res) => {
 	const { email, name, password } = req.body;
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		entries: 0,
-		joined: new Date()
+	db('users')
+		.returning('*')
+		.insert({
+			email: email,
+			name: name,
+			joined: new Date()
+	}).then(user => {
+
+		res.json(user[0]);
+
 	})
-	res.json(database.users[database.users.length - 1]);
+	.catch(err => res.status(400).json('Unable to register'))
 })
 
 app.get('/profile/:id', (req,res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		} 
+	
+	db.select('*').from('users').where({id})
+	.then(user => {
+		if (user.length) { 
+
+		res.json((user[0]))
+		} else {
+			
+		res.status(400).json('Not found')
+		}
 	})
-	if (!found) {
-		res.status(404).json('Not found');
-	}
+	.catch(err => res.status(400).json('error getting user'))
+
+
 })
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		} 
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
 	})
-	if (!found) {
-		res.status(404).json('Not found here');
-	}
+	.catch(err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3000, () => {
